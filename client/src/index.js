bf715891d94ad54400fdecfa0e92d8a0722060a9
Brennen <ketron@outlook.com>
2017-05-01 17:10:16 -0700

    yarn added stuff

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,12 @@ import App from './App'
 import './index.css'
 import {Provider} from 'react-redux'
 import store from './store'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+injectTapEventPlugin();
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
+  <MuiThemeProvider><Provider store={store}><App /></Provider></MuiThemeProvider>,
   document.getElementById('root')
 )
