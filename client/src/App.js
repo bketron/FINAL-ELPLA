3fd841d074d6d17eb33ca9e65808d54823100653
Charles DeLong <c.thecheesedelong@gmail.com>
2017-05-08 15:07:30 -0700

    resolved conflict

--- client/src/App.js
+++ client/src/App.js
@@ -14,20 +14,7 @@ class App extends Component {
         </div>
       )
     }
-  
-=======
-// import FilterChips from './CharlesComponents/FilterChips'
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-      	<FilterPanel />
-      </div>
-    )
-  }
 }
-
 
 const mapStateToProps = function(appState) {
   return {
